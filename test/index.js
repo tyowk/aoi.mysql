b74ebd63972583d8b4092454407382ed05875d8a
Tyowk <tyowk.personal@gmail.com>
2024-09-30 14:28:21 +0700

    Create index.js

---
+++ test/index.js
@@ -0,0 +1,10 @@
+const { AoiClient } = require('aoi.js');
+const { Database } = require('../../src/index.js');
+
+const client = new AoiClient({
+    
+});
+
+const db = new Database(client, {
+    
+});
