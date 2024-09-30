57a26cdb880bf3ef6296b0f1b7ff14de6df4a290
Tyowk <tyowk.personal@gmail.com>
2024-09-30 14:40:26 +0700

    Update and rename test/set-variable.js to test/commands/set-variable.js

--- test/commands/set-variable.js
+++ test/commands/set-variable.js
@@ -1,9 +1,13 @@
 module.exports = [{
+    
     name: 'set',
     code: `variable value set to: **$nonEscape[$message]**
     $setVar[test;$nonEscape[$message];test]`
+
 },{
+    
     name: 'reset',
     code: `variable value has been reset to default.
     $setVar[test;value;test]`
+    
 }]
