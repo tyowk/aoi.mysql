385dc274cc2df3e0cf5893126a42d4c9a32bbe65
Tyowk <tyowk.personal@gmail.com>
2024-09-30 14:40:04 +0700

    Create set-variable.js

---
+++ test/set-variable.js
@@ -0,0 +1,9 @@
+module.exports = [{
+    name: 'set',
+    code: `variable value set to: **$nonEscape[$message]**
+    $setVar[test;$nonEscape[$message];test]`
+},{
+    name: 'reset',
+    code: `variable value has been reset to default.
+    $setVar[test;value;test]`
+}]
