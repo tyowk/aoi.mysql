9b5b48f4e5521ddf5485efb462245164c5569b57
Tyowk <tyowk.personal@gmail.com>
2024-09-30 14:30:42 +0700

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,10 +1,16 @@
 const { AoiClient } = require('aoi.js');
-const { Database } = require('../../src/index.js');
+const { Database } = require('../src/index.js');
+require('dotenv').config();
 
 const client = new AoiClient({
-    
+    token: process.env.TOKEN,
+    prefix: process.env.PREFIX,
+    intents: ['Guilds', 'GuildMessages', 'GuildMembers', 'MessageContent'],
+    events: ['onMessage', 'onInteractionCreate'],
+    disableAoiDB: true
 });
 
 const db = new Database(client, {
-    
+    url: process.env.DATABASE,
+    tables: ['test']
 });
