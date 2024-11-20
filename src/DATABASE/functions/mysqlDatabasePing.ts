7caaf5d2d60f5d35bbc70187e3819d5b8a5927f2
Tyowk <tyowk.personal@gmail.com>
2024-11-20 14:37:22 +0700

    Delete src/DATABASE directory

--- src/DATABASE/functions/mysqlDatabasePing.ts
+++
@@ -1,7 +0,0 @@
-module.exports = async (d: any) => {
-    const data = d.util.aoiFunc(d);
-    data.result = await d.client.mysql.db.avgPing();
-    return {
-        code: d.util.setCode(data)
-    }
-}
--- src/DATABASE/functions/mysqlIsVariableExist.ts
+++
@@ -1,8 +0,0 @@
-module.exports = (d: any) => {
-  const data = d.util.aoiFunc(d);
-  const [variable,table = d.client.mysql.tables[0]] = data.inside.splits;
-  data.result = d.client.variableManager.has(variable,table)
-  return {
-    code: d.util.setCode(data)
-  }
-}
