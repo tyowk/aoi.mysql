e4036cead7b59c429831f156bd33fd9c91f61ca6
Tyowk <tyowk.personal@gmail.com>
2024-11-24 08:49:42 +0700

    Update mysqlGlobalCooldown.js

--- src/functions/mysqlGlobalCooldown.js
+++ src/functions/mysqlGlobalCooldown.js
@@ -1,6 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Time_1 = require("../classes/Time");
+const { Time } = require('aoi.js/src/core/Time');
+
 module.exports = async (d) => {
     const { code } = d.command;
     const inside = d.unpack();
@@ -12,14 +11,14 @@ module.exports = async (d) => {
     let cooldown = await d.client.mysql.get("__aoijs_vars__", "cooldown", `${d.command.name}_${d.author.id}`);
     cooldown = cooldown?.value;
     if (!cooldown) {
-        cooldown = Date.now() + Time_1.Time.parse(time).ms;
+        cooldown = Date.now() + Time.parse(time).ms;
         d.client.mysql.set("__aoijs_vars__", "cooldown", `${d.command.name}_${d.author.id}`, cooldown);
     }
     else if (Date.now() < cooldown) {
         if (errorObject.trim() === "") {
         }
         else {
-            const { object, humanize, toString } = Time_1.Time.format(cooldown - Date.now());
+            const { object, humanize, toString } = Time.format(cooldown - Date.now());
             errorObject = errorObject
                 .replaceAll("%time%", humanize())
                 .replaceAll("%year%", object.years)
@@ -37,7 +36,7 @@ module.exports = async (d) => {
         error = true;
     }
     else {
-        cooldown = Date.now() + Time_1.Time.parse(time).ms;
+        cooldown = Date.now() + Time.parse(time).ms;
         d.client.mysql.set("__aoijs_vars__", "cooldown", `${d.command.name}_${d.author.id}`, cooldown);
     }
     return {
